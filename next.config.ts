9e3b4f08eb2ea19a53d9dcdeeddc0dc7bc8e7ec0
Enes Demiryürek <enesdmryurek@gmail.com>
2026-07-22 13:44:19 +0300

    fix: correct basePath to optik-i

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Static export — hosting'e yüklemek için
   output: "export",
-  basePath: "/optik-l", // <-- CSS ve asset yollarının kaymaması için bu şart!
+  basePath: "/optik-i", // <-- CSS ve asset yollarının kaymaması için bu şart!
   trailingSlash: true,
 
   images: {
